841e2db66a53b0b647d4fa30929149af383a04f1
Toma1304 <francesco.tomaciello@gmail.com>
2024-12-17 14:53:13 +0100

    feat:aggiunta funzione deleteTodo

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ export const addTodo = (title, category) => {
     return newTodo;
 }
 
+export const deleteTodo = (id) => {
+    const index = todos.findIndex(todo => todo.id === id);
+    if(index !== -1) {
+        const deletedTodo = todos.splice(index, 1);
+        return deletedTodo[0];
+    }
+    return null;
+}
+
